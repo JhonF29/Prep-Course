73f1668cf77b10d7663d3a3cb5ec836e44f81257
Jhon Fredy González <jjoonnffrreeddyy123@hotmail.com>
2022-11-16 14:56:41 -0500

    Primer punto de teoria 03

---
+++ 03-JS-II/homework/teorico03.js
@@ -0,0 +1,9 @@
+//Homework 03, teorico, en este apartado eplicare, el for es como un ir donde tambien podemos tener condicion 
+//Este consta de tres elementos,la variable sobre la cual se repetira el bucle del for, la expresion condicional
+//La cual hace que se repita el bucle hasta quue esta sea false, y por ultimo la que incrementa la variable
+//&& este operador logico funciona de la siguiente forma, primero evalua dos escenarios y si los dos son ciertos
+//Dara como resultado true, si uno o ambos de ellos es falso dara como resultado false
+//|| conocido como or, sirve para evaluar una a uno de las variable puestas en cuestion, donde cuestiona la primera o
+// la segunda o la tercera en orden pero siempre evaluando para entrar a la funcion con un ejemplo un if 
+//! discriminante o negacion, en el caso del ===, este evalua a los das partes y cada factor de esta, y si lo reemplazamos con
+// !==, seria la parte contraria a lo que se establecion con la triple igualdad
